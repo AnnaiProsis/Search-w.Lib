85b6ec0c7620fd1f8cb0d5d23ef02b36bb09e28f
Anna <annak@iprosis.com>
2020-01-09 17:52:09 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -1,21 +1,95 @@
 (function()  {
-     let d3Script = document.createElement('script');
+    // let d3Script = document.createElement('script');
     // / d3Script.src = 'https://openui5.hana.ondemand.com/1.48.4/resources/sap-ui-core.js';
-    d3Script.async = false;
-	document.head.appendChild(d3Script);
+    //d3Script.async = false;
+	//document.head.appendChild(d3Script);
 	
 	let tmpl = document.createElement('template');
 	tmpl.innerHTML = `
-	
-	`;
-	d3Script.onload = () => 
+	<style>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
 
-    customElements.define('com-sap-teched-gauge-solution-exe5', class Gauge extends HTMLElement {
-	//class ISearch extends HTMLElement {
+body {
+    background-color: #232C31;
+}
+
+.search-bar {
+    position: absolute;
+    top: 100px;
+    left: 100px;
+}
+
+input {
+    position: relative;
+    padding-left: 20px;
+    font: 1em "Helvetica", sans-serif;
+    width: 200px;
+    height: 50px;
+    border-radius: 5px 0px 0px 5px;
+    border: none;
+    background-color: #151A1D;
+    outline: none;
+    color: white;
+}
+
+.search-icon {
+    position: absolute;
+    left: 200px;
+    width: 50px;
+    height: 50px;
+    border: none;
+    border-radius: 0px 5px 5px 0px;
+    background-color: #42D8D3;
+}
+
+.search-icon {
+    cursor: pointer;
+    font-size: 1em;
+}
+
+.autocomplete-items {
+	position: absolute;
+	border: 1px solid #d4d4d4;
+	border-bottom: none;
+	border-top: none;
+	z-index: 99;
+	/*position the autocomplete items to be the same width as the container:*/
+	top: 100%;
+	left: 0;
+	right: 0;
+	}
+	.autocomplete-items div {
+	padding: 10px;
+	cursor: pointer;
+	background-color: #fff; 
+	border-bottom: 1px solid #d4d4d4; 
+	}
+	/*when hovering an item:*/
+	.autocomplete-items div:hover {
+	background-color: #e9e9e9; 
+	}
+	/*when navigating through the items using the arrow keys:*/
+	.autocomplete-active {
+	background-color: DodgerBlue !important; 
+	color: #ffffff; 
+	
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<div class="search-bar">
+    <input type="text" name="search" placeholder="Search..." onfocus="this.placeholder=''" onblur="this.placeholder='Search...'"></input>
+    <button class="search-icon"><span class="search-btn fa fa-search"></span></button>
+</div>
+
+	`;
+	class ISearch extends HTMLElement {
 		constructor() {
-			var view = sap.ui.xmlview({
-				viewContent: '<mvc:View height="100%" xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m" > <Page title="OpenUI5 Bind aggregation example" class="sapUiContentPadding" showNavButton="false" > <subHeader> <Toolbar> <SearchField id="search-field"/> </Toolbar> </subHeader>  </Page> </mvc:View>'
-			});
+			// var view = sap.ui.xmlview({
+			// 	viewContent: '<mvc:View height="100%" xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m" > <Page title="OpenUI5 Bind aggregation example" class="sapUiContentPadding" showNavButton="false" > <subHeader> <Toolbar> <SearchField id="search-field"/> </Toolbar> </subHeader>  </Page> </mvc:View>'
+			// });
 			super();
 			let shadow = this.attachShadow({ mode: 'open' });
 			shadow.appendChild(tmpl.content.cloneNode(true));
@@ -210,8 +284,7 @@
 			getSelectedDimension(){
 				return this.dim;
 			}
-		});
-		})();
+
 			// chooseData(){
 			// 	if (this.display == 'k'){
 			// 		this.dataModel = this.dataModel0;
@@ -224,11 +297,11 @@
 			// 	}
 			// }
 
-//	} //end of class
+	} //end of class
 
 	/* Define web component - input: tag and class */
-//	customElements.define('com-iprosis-sample-search', ISearch);
-//})();
+	customElements.define('com-iprosis-sample-search', ISearch);
+})();
 	// d3Script.onload = () => 
     // customElements.define('com-iprosis-search', class ISearch extends HTMLElement {
 
@@ -389,4 +462,8 @@
 	// 		//	this._shadowRoot.getElementById("showbutton_val").value = changedProperties["showbutton"];
 	// 		}
 	// 	}
+		
+
+	
+
 	// }); //end of class
