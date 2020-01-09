28802d3074344065c664c633acd1567e6ce8ede7
Anna <annak@iprosis.com>
2020-01-09 14:49:02 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -1,6 +1,6 @@
 (function()  {
     let d3Script = document.createElement('script');
-    d3Script.src = 'https://openui5.hana.ondemand.com/resources/sap-ui-core.js';
+    d3Script.src = 'https://openui5.hana.ondemand.com/1.48.4/resources/sap-ui-core.js';
     d3Script.async = false;
 	document.head.appendChild(d3Script);
 	
@@ -29,7 +29,7 @@
 
 		constructor() {
 			var view = sap.ui.xmlview({
-				viewContent: '<mvc:View height="100%" xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m" > <Page title="OpenUI5 Bind aggregation example" class="sapUiContentPadding" showNavButton="false" > <subHeader> <Toolbar> <SearchField id="search-field"/> </Toolbar> </subHeader> </Page> </mvc:View>'
+				viewContent: '<mvc:View height="100%" xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m" > <Page title="" class="sapUiContentPadding" showNavButton="false" > <subHeader> <Toolbar> <SearchField id="search-field"/> </Toolbar> </subHeader> </Page> </mvc:View>'
 				});
 			//Constants
             // if (!window._d3){
