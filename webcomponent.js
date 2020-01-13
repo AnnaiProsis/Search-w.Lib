426b561d48659587edf830a7ef0cd47c41e18d95
Anna <annak@iprosis.com>
2020-01-13 10:35:32 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -66,10 +66,10 @@ $(function() {
 			super();
 			let shadow = this.attachShadow({ mode: 'open' });
 			shadow.appendChild(tmpl.content.cloneNode(true));
-			this.x = shadow.querySelector('#autocomplete')
+			//this.x = shadow.querySelector('#autocomplete')
 			// document.addEventListener('DOMContentLoaded', function() {
 			// 	var elems = document.querySelectorAll('.autocomplete');
-			// 	var instances = M.Autocomplete.init(elems, options);
+			 	var instances = M.Autocomplete.init(elems, options);
 			//   });		
 		}  // end of constructor
 
@@ -99,7 +99,7 @@ $(function() {
 		/* setter of datalist to autocomplete*/
 		setMembers(newdata,displayType) {
 			 this.display = displayType;
-			 M.Autocomplete.init(this.x, newdata);
+			 M.Autocomplete.init(newdata,options );
 			
 			}
 
