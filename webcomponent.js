a953f1729deb305698aa884334188abd1a797ee7
Anna <annak@iprosis.com>
2020-01-27 15:22:36 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -12,27 +12,27 @@
 		alert("Image loaded, size");
 	  };
 
-  let jqScript = document.createElement("script");
-  jqScript.src = "https://code.jquery.com/jquery-3.4.1.slim.js";
-  jqScript.async = false;
-  document.head.appendChild(jqScript);
+//   let jqScript = document.createElement("script");
+//   jqScript.src = "https://code.jquery.com/jquery-3.4.1.slim.js";
+//   jqScript.async = false;
+//   document.head.appendChild(jqScript);
 
 
-    let materilizeCss = document.createElement("link");
-    materilizeCss.rel = "stylesheet";
-    materilizeCss.type = "text/css";
-    materilizeCss.href =
-      "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css";
+//     let materilizeCss = document.createElement("link");
+//     materilizeCss.rel = "stylesheet";
+//     materilizeCss.type = "text/css";
+//     materilizeCss.href =
+//       "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css";
   
-    document.head.appendChild(materilizeCss);
+//     document.head.appendChild(materilizeCss);
 
-    let materilizeIcons = document.createElement("link");
-    materilizeIcons.rel = "stylesheet";
-    materilizeIcons.type = "text/css";
-    materilizeIcons.href =
-      "https://fonts.googleapis.com/icon?family=Material+Icons";
+//     let materilizeIcons = document.createElement("link");
+//     materilizeIcons.rel = "stylesheet";
+//     materilizeIcons.type = "text/css";
+//     materilizeIcons.href =
+//       "https://fonts.googleapis.com/icon?family=Material+Icons";
   
-    document.head.appendChild(materilizeIcons);
+//     document.head.appendChild(materilizeIcons);
   
 	
 	let tmpl = document.createElement('template');
@@ -69,8 +69,8 @@
 
 
 	`;
-	const csss = new CSSStyleSheet();
-	document.adoptedStyleSheets = [csss];
+	// const csss = new CSSStyleSheet();
+	// document.adoptedStyleSheets = [csss];
 	
 
 	materilizeScript.onload = () => 
@@ -83,10 +83,10 @@
 			super();
 			let shadow = this.attachShadow({ mode: 'open' });
 			shadow.appendChild(tmpl.content.cloneNode(true));
-			shadow.appendChild(materilizeCss);
-			shadow.appendChild(materilizeIcons);
+			// shadow.appendChild(materilizeCss);
+			// shadow.appendChild(materilizeIcons);
 			shadow.appendChild(materilizeScript);
-			shadow.appendChild(jqScript);
+			//shadow.appendChild(jqScript);
 			this.adoptedStyleSheets = [csss];	
 			var that = this;
 			//this.x = shadow.querySelector('.autocomplete');
